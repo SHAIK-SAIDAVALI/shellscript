5db52c0e2e581defaa69cf4b223569e3a2e23ac1
shaiksaidavali <shaiksaidavali223@gmail.com>
2026-05-10 19:41:54 +0530

    hello world

---
+++ 2.conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "saida: hi sandeep, how are you ?"
+echo "sandeep : hi saida , i am fine . what about you ?"
+echo "saida: I am fine. what is the plan for this weekend ?"
+echo "sandeep : Nothing specific, will play cricket"
