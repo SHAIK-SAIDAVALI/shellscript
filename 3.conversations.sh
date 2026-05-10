22c3b779c1dc62b59c5b4951360e2421c3166f82
shaiksaidavali <shaiksaidavali223@gmail.com>
2026-05-10 19:49:24 +0530

    hello world

--- 3.conversations.sh
+++ 3.conversations.sh
@@ -4,6 +4,6 @@ PERSON1=MODI
 PERSON2=RAHUL
 
 echo "$PERSON1: hi $PERSON2, how are you ?"
-echo "$PERSON2 : hi saida , i am fine . what about you ?"
+echo "$PERSON2 : hi $PERSON1 , i am fine . what about you ?"
 echo "$PERSON1: I am fine. what is the plan for this weekend ?"
 echo "$PERSON2 : Nothing specific, will play cricket"
