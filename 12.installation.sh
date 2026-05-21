9ac26d901ca5826e2296b36d562476854d9c2241
shaiksaidavali <shaiksaidavali223@gmail.com>
2026-05-21 18:56:36 +0530

    hello world

---
+++ 12.installation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ] then;
+    echo "ERROR:: please proceed further with root acess"
+    exit 1
+fi
+
+dnf install nginx -y
+
+if [ $? -ne 0  ] then;
+    echo "Error:: installation failed"
+    exit 1
+else
+    echo "success" 
+
+fi
+
